eeaae61ff5dd4bca01f50270108f47d92f366a4e
Ranjeet <ranjeet@gmail.com>
2019-10-30 17:35:58 +0530

    timezone package created

---
+++ src/TimezonesServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace ShoutLara\Timezones;
+
+use Illuminate\Support\ServiceProvider;
+
+class TimezonesServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        include __DIR__.'/routes/web.php';
+        $this->app->make('ShoutLara\Timezones\TimezonesController');
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+        __DIR__.'/views' => base_path('resources/views/shoutlara/timezones'),
+    ]);
+       $this->loadViewsFrom(__DIR__.'/views', 'timezones');
+    }
+}
---
+++ src/routes/web.php
@@ -0,0 +1,4 @@
+<?php
+
+Route::get('timezones/{timezone?}', 'shoutlara\timezones\TimezonesController@index');
+
